f6c277c28201e804f509dc8fb610cea8fa2f8811
Ido Shimshi <idshimshi@gmail.com>
2023-04-12 15:57:13 +0300

    task2 and 3 added, still figuring 3

--- Assignment1/task2.py
+++ Assignment1/task2.py
@@ -1,12 +1,12 @@
 from sklearn import datasets
+from sklearn.model_selection import train_test_split
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.metrics import mean_squared_error
 
 def err(X,Y,x):
     return (np.linalg.norm((X @ x) - Y)**2) / X.shape[0]
 
-def solve_least_squares(X,Y,delta=10,eps=0.01):
+def solve_least_squares(X,Y,delta=0.1,eps=0.5):
     retx = []
     XTY = X.T @ Y
     XTX = X.T @ X
@@ -21,15 +21,22 @@ def solve_least_squares(X,Y,delta=10,eps=0.01):
 X,Y = datasets.load_diabetes(return_X_y=True)
 X = np.array(X)
 Y = np.array([Y]).T
-listx = solve_least_squares(X,Y)
-print(err(X,Y,listx[-1]))
-errors = [err(X,Y,x) for x in listx]
-plt.plot(errors)
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+
+
+listx = solve_least_squares(X_train,Y_train)
+errors_train = [err(X_train,Y_train,x) for x in listx]
+errors_test = [err(X_test,Y_test,x) for x in listx]
+x_index = np.arange(start=0, stop=len(listx), step=1)
+plt.figure()
+plt.plot(x_index, errors_train, label='train err')
+plt.plot(x_index, errors_test, label='test err')
 
 # Add labels and title
 plt.xlabel('Step Index')
 plt.ylabel('Error')
 plt.title('Error of each step')
+plt.legend()
 
 # Show plot
 plt.show()
---
+++ Assignment1/task3.py
@@ -0,0 +1,46 @@
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+import numpy as np
+import matplotlib.pyplot as plt
+
+def err(X,Y,x):
+    return (np.linalg.norm((X @ x) - Y)**2) / X.shape[0]
+
+def solve_least_squares(X,Y,delta=0.1,eps=0.5):
+    retx = []
+    XTY = X.T @ Y
+    XTX = X.T @ X
+    derivative = lambda x: ((XTX @ x) - (XTY))
+    xk = np.random.rand(X.shape[1],1)
+    while (np.linalg.norm(derivative(xk))) > delta:
+        retx.append(xk)
+        xk = xk - eps*derivative(xk)
+    retx.append(xk)
+    return retx
+
+X,Y = datasets.load_diabetes(return_X_y=True)
+X = np.array(X)
+Y = np.array([Y]).T
+errors_train = []
+errors_test = []
+for i in range(10):
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+    listx = solve_least_squares(X_train,Y_train)
+    err_train = [err(X_train,Y_train,x) for x in listx]
+    err_test = [err(X_test,Y_test,x) for x in listx]
+    errors_train.append(err_train)
+    errors_test.append(err_test)
+
+# x_index = np.arange(start=0, stop=len(listx), step=1)
+# plt.figure()
+# plt.plot(x_index, errors_train, label='train err')
+# plt.plot(x_index, errors_test, label='test err')
+
+# # Add labels and title
+# plt.xlabel('Step Index')
+# plt.ylabel('Error')
+# plt.title('Error of each step')
+# plt.legend()
+
+# # Show plot
+# plt.show()
